745189cc8463baf57ad0cef55463e1bc27e9226d
Fethiye <fethiye0623@gmail.com>
2023-07-16 13:01:54 +0200

    TrendyolTestCase updated..

--- src/test/java/cw/tests/TrendyolTestCase.java
+++ src/test/java/cw/tests/TrendyolTestCase.java
@@ -4,9 +4,16 @@ import cw.pages.TrendyolBasePage;
 import cw.pages.TrendyolHomePage;
 import cw.utilities.ConfigurationReader;
 import cw.utilities.Driver;
+import cw.utilities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class TrendyolTestCase {
+import java.time.Duration;
+
+public class TrendyolTestCase extends TestBase {
 
 
 
@@ -23,22 +30,26 @@ Test the product you added to the cart
     @Test
     public void test() {
 
-        Driver.getDriver().get(ConfigurationReader.getProperty("trendyolUrl"));
+        driver.get("https://www.trendyol.com/");
 
         //click cookies..
-       // TrendyolHomePage homePage = new TrendyolHomePage();
+        // TrendyolHomePage homePage = new TrendyolHomePage();
 
-        TrendyolBasePage basePage=new TrendyolBasePage();
+        WebElement cookies = driver.findElement(By.xpath("//button[.='Alle akzeptieren']"));
 
-        basePage.cookiesAccept();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(cookies));
 
-      //  homePage.cookiesAccept();
+        cookies.click();
+
+
+        //  homePage.cookiesAccept();
 
 //        search for any product..
         //homePage.searchProduct("Schuhe");
 
 //        close rabatt-window..
-       // homePage.rabattWE.click();
+        // homePage.rabattWE.click();
 
 
 //        click on the first product on page 2 of the search results
